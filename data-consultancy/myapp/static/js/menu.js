86000035e3d8afb3eb0f589c30373186aa5f1160
Ivan Lopes <ivan.alonso@globalhitss.com>
2024-08-02 15:11:23 -0300

    Ajustes no component js menu, mudanças no texto e layout do home

--- data-consultancy/myapp/static/js/menu.js
+++ data-consultancy/myapp/static/js/menu.js
@@ -1,13 +1,21 @@
 "use strict";
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    if (menu) {
-        menu.classList.toggle('active');
-    }
-}
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
-    if (menuToggle) {
-        menuToggle.addEventListener('click', toggleMenu);
+    const menu = document.querySelector('.menu');
+    const body = document.querySelector('body');
+    // Verifica se os elementos existem antes de adicionar eventos
+    if (menuToggle && menu) {
+        menuToggle.addEventListener('click', (event) => {
+            event.stopPropagation(); // Previne que o clique no menuToggle feche o menu
+            menu.classList.toggle('active');
+            body.classList.toggle('menu-open');
+        });
     }
+    document.addEventListener('click', (event) => {
+        // Verifica se o clique foi fora do menu e do menuToggle
+        if (menu.classList.contains('active') && !menu.contains(event.target) && !menuToggle.contains(event.target)) {
+            menu.classList.remove('active');
+            body.classList.remove('menu-open');
+        }
+    });
 });
--- data-consultancy/myapp/static/ts/menu.ts
+++ data-consultancy/myapp/static/ts/menu.ts
@@ -1,13 +1,22 @@
-function toggleMenu(): void {
-    const menu = document.querySelector('.menu') as HTMLElement;
-    if (menu) {
-        menu.classList.toggle('active');
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle') as HTMLElement;
-    if (menuToggle) {
-        menuToggle.addEventListener('click', toggleMenu);
+    const menu = document.querySelector('.menu') as HTMLElement;
+    const body = document.querySelector('body') as HTMLElement;
+
+    // Verifica se os elementos existem antes de adicionar eventos
+    if (menuToggle && menu) {
+        menuToggle.addEventListener('click', (event) => {
+            event.stopPropagation(); // Previne que o clique no menuToggle feche o menu
+            menu.classList.toggle('active');
+            body.classList.toggle('menu-open');
+        });
     }
+
+    document.addEventListener('click', (event) => {
+        // Verifica se o clique foi fora do menu e do menuToggle
+        if (menu.classList.contains('active') && !menu.contains(event.target as Node) && !menuToggle.contains(event.target as Node)) {
+            menu.classList.remove('active');
+            body.classList.remove('menu-open');
+        }
+    });
 });
